69930b213d401d1ee6a771dc112bbfde0cdb489a
vitalis <vitalismutwiri2gmail.com>
2022-12-31 00:27:37 +0300

    added product

--- src/components/Product.js
+++ src/components/Product.js
@@ -11,7 +11,7 @@ function Product() {
       {
         data.filter((ad) => ad.id === id).map((item) => (
           <div key={item.id}>
-            <div className="container my-5">
+            <div className="container">
               <div className="row">
                 <div className="col-md-5">
                   <div className="main-img">
@@ -74,45 +74,34 @@ function Product() {
                 </div>
               </div>
             </div>
-            <div className="container similar-products my-4">
-              <hr />
-              <p className="display-5">Similar Products</p>
-
-              <div className="row">
-                <div className="col-md-3">
-                  <div className="similar-product">
-                    <img className="w-100" src="https://source.unsplash.com/gsKdPcIyeGg" alt="Preview" />
-                    <p className="title">Lovely black dress</p>
-                    <p className="price">$100</p>
-                  </div>
-                </div>
-                <div className="col-md-3">
-                  <div className="similar-product">
-                    <img className="w-100" src="https://source.unsplash.com/sg_gRhbYXhc" alt="Preview" />
-                    <p className="title">Lovely Dress with patterns</p>
-                    <p className="price">$85</p>
-                  </div>
-                </div>
-                <div className="col-md-3">
-                  <div className="similar-product">
-                    <img className="w-100" src="https://source.unsplash.com/gJZQcirK8aw" alt="Preview" />
-                    <p className="title">Lovely fashion dress</p>
-                    <p className="price">$200</p>
-                  </div>
-                </div>
-                <div className="col-md-3">
-                  <div className="similar-product">
-                    <img className="w-100" src="https://source.unsplash.com/qbB_Z2pXLEU" alt="Preview" />
-                    <p className="title">Lovely red dress</p>
-                    <p className="price">$120</p>
-                  </div>
-                </div>
-              </div>
-            </div>
 
           </div>
         ))
       }
+
+      <div className="container similar-products my-4">
+        <hr />
+        <p className="display-5">Similar Products</p>
+
+        <div className="row">
+          {
+              data.slice(0, 4).map((item) => (
+                <>
+
+                  <div className="col-md-3" key={item.id}>
+                    <div className="similar-product">
+                      <img className="w-100" src={`${item.image}`} alt="Preview" />
+                      <p className="title">Lovely black dress</p>
+                      <p className="price">$100</p>
+                    </div>
+                  </div>
+                </>
+              ))
+                }
+        </div>
+
+      </div>
+
     </div>
   );
 }
